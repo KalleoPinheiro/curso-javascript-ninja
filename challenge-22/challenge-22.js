2e3e9d174ec89baf1e24e4d3a0b5d1e3d30109ec
Kalleo Pinheiro <kalleopinheiro@hotmail.com>
2019-02-22 23:21:20 -0300

    Init challenge-22 resolution

--- challenge-22/challenge-22.js
+++ challenge-22/challenge-22.js
@@ -1,9 +1,18 @@
+(() => {
+  'use strict';
   /*
   Crie dois objetos, que serão duas pessoas. Cada um deve ter as propriedades
   `name` e `lastName`, preenchidos com o nome e sobrenome da pessoa.
   */
-  // ?
+  const kalleo = {
+    name: 'Kalléo',
+    lastName: 'Pinheiro'
+  };
 
+  const jeiciele = {
+    name: 'Jeiciele',
+    lastName: 'Silva'
+  };
   /*
   Agora crie uma função chamada `getFullName` que retorne as propriedades
   `name` e `lastName` dos objetos acima, formando um nome completo.
@@ -15,8 +24,14 @@
   pessoas que foram criadas anteriormente, passando as pessoas acima como
   contexto da função. Use um console.log por pessoa.
   */
-  console.log( 'O nome das pessoas é:' );
-  // ?
+  console.log('O nome das pessoas é:');
+
+  function getFullName() {
+    return `${this.name} ${this.lastName}`;
+  }
+
+  console.log(getFullName.call(kalleo));
+  console.log(getFullName.call(jeiciele));
 
   /*
   Crie uma função chamada `sum`. Essa função pode receber uma lista de
@@ -31,7 +46,7 @@
   Mostre no console que a função acima funciona, invocando-a em 3 console.log
   diferentes, com quantidades variáveis de parâmetros passados.
   */
-  console.log( '\nSomar alguns números:' );
+  console.log('\nSomar alguns números:');
   // ?
 
   /*
@@ -44,7 +59,7 @@
   /*
   Mostre no console o valor entrado pelo usuário:
   */
-  console.log( '\nEntrada do usuário:' );
+  console.log('\nEntrada do usuário:');
   // ?
 
   /*
@@ -52,19 +67,20 @@
   e remove tudo o que não for número, retornando um array somente com os números
   da string. Mostre a representação em string dessa função no console.
   */
-  console.log( '\nFunção que limpa entrada do usuário (somente números):' );
+  console.log('\nFunção que limpa entrada do usuário (somente números):');
   // ?
 
   /*
   Usando a função acima, faça a limpeza dos valores entrados pelo usuário,
   atribuindo o resultado à uma variável `numbers`.
   */
-  console.log( '\nEntrada do usuário limpa. Somente números:' );
+  console.log('\nEntrada do usuário limpa. Somente números:');
   // ?
 
   /*
   Agora com o array de números, utilize a função `sum` para somar todos os
   números desse array e mostre o resultado no console.
   */
-  console.log( '\nSomar números entrados pelo usuário:' );
+  console.log('\nSomar números entrados pelo usuário:');
   // ?
+})();
